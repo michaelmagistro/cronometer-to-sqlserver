e933cf833663aa695cd38f2ac343ad419a049d5a
Michael Magistro <michaelmagistro@users.noreply.github.com>
2025-12-29 16:20:00 -0700

    add scheduling to the job

--- sql_jobs/Cronometer_RefreshDB_Job.sql
+++ sql_jobs/Cronometer_RefreshDB_Job.sql
@@ -8,11 +8,11 @@ BEGIN
 END
 GO
 
-/****** Object:  Job [Cronometer_RefreshDB]    Script Date: Sunday/December 28/2025 6:33:06 PM ******/
+/****** Object:  Job [Cronometer_RefreshDB]    Script Date: Monday/December 29/2025 11:26:24 AM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: Sunday/December 28/2025 6:33:06 PM ******/
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: Monday/December 29/2025 11:26:24 AM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
@@ -32,7 +32,7 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Cronometer_RefreshDB',
 		@category_name=N'[Uncategorized (Local)]', 
 		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Load_Biometrics]    Script Date: Sunday/December 28/2025 6:33:06 PM ******/
+/****** Object:  Step [Load_Biometrics]    Script Date: Monday/December 29/2025 11:26:24 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Biometrics', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
@@ -48,7 +48,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Bio
 		@output_file_name=N'C:\SQLLogs\Cronometer_Step1_Biometrics.log', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Load_DailySummary]    Script Date: Sunday/December 28/2025 6:33:06 PM ******/
+/****** Object:  Step [Load_DailySummary]    Script Date: Monday/December 29/2025 11:26:24 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_DailySummary', 
 		@step_id=2, 
 		@cmdexec_success_code=0, 
@@ -64,7 +64,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Dai
 		@output_file_name=N'C:\SQLLogs\Cronometer_Step2_DailySummary.log', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Load_Exercises]    Script Date: Sunday/December 28/2025 6:33:06 PM ******/
+/****** Object:  Step [Load_Exercises]    Script Date: Monday/December 29/2025 11:26:24 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Exercises', 
 		@step_id=3, 
 		@cmdexec_success_code=0, 
@@ -80,7 +80,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Exe
 		@output_file_name=N'C:\SQLLogs\Cronometer_Step3_Exercises.log', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Load_Fasts]    Script Date: Sunday/December 28/2025 6:33:06 PM ******/
+/****** Object:  Step [Load_Fasts]    Script Date: Monday/December 29/2025 11:26:24 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Fasts', 
 		@step_id=4, 
 		@cmdexec_success_code=0, 
@@ -96,7 +96,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Fas
 		@output_file_name=N'C:\SQLLogs\Cronometer_Step4_Fasts.log', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Load_Notes]    Script Date: Sunday/December 28/2025 6:33:06 PM ******/
+/****** Object:  Step [Load_Notes]    Script Date: Monday/December 29/2025 11:26:24 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Notes', 
 		@step_id=5, 
 		@cmdexec_success_code=0, 
@@ -112,7 +112,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Not
 		@output_file_name=N'C:\SQLLogs\Cronometer_Step5_Notes.log', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Load_Servings]    Script Date: Sunday/December 28/2025 6:33:06 PM ******/
+/****** Object:  Step [Load_Servings]    Script Date: Monday/December 29/2025 11:26:24 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Servings', 
 		@step_id=6, 
 		@cmdexec_success_code=0, 
@@ -130,6 +130,20 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load_Ser
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'HourlyLoad', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=8, 
+		@freq_subday_interval=1, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20251229, 
+		@active_end_date=99991231, 
+		@active_start_time=63000, 
+		@active_end_time=213059, 
+		@schedule_uid=N'8525356d-02f3-49a9-8f6b-f95f8a192c57'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 COMMIT TRANSACTION
@@ -138,6 +152,9 @@ QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:
 GO
+
+
+
 
 
 
