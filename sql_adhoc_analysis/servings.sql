72caf9bc32b22b7d05f3f89b333da7403b743351
Michael Magistro <michaelmagistro@users.noreply.github.com>
2026-01-17 07:03:03 -0700

    add small query to check nutrient amt intake

--- sql_adhoc_analysis/servings.sql
+++ sql_adhoc_analysis/servings.sql
@@ -1,3 +1,7 @@
+/*********************************************************************
+BEGIN: SQL Adhoc Analysis
+*********************************************************************/
+
 SELECT SUBSTRING(amount, 0, CHARINDEX(' ', amount)) AS Amt,
 	CASE WHEN SUBSTRING(SUBSTRING(amount, CHARINDEX(' ', amount) + 1, LEN(amount)),0,2) = 'x '
 		THEN SUBSTRING(amount, CHARINDEX(' ', amount) + 3, LEN(amount))
@@ -6,3 +10,22 @@ SELECT SUBSTRING(amount, 0, CHARINDEX(' ', amount)) AS Amt,
 	*
 FROM servings
 ORDER BY Day DESC, Time DESC
+
+/*----------------------------------------------------------------------
+END: SQL Adhoc Analysis
+----------------------------------------------------------------------*/
+
+/*********************************************************************
+BEGIN: Simple How Much of a Nutrient
+*********************************************************************/
+
+select food_name, day
+, sum(cast(SUBSTRING(amount, 0, CHARINDEX(' ', amount)) as float)) as amt
+from servings
+where Food_Name like '%creatin%'
+group by food_name, day
+order by day desc
+
+/*----------------------------------------------------------------------
+END: Simple How Much of a Nutrient
+----------------------------------------------------------------------*/
